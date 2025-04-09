3b548dfcc72ae48f94f64ebf64c5100765cbea0b
Sasank5716 <sasanklama5716@gmail.com>
2025-04-09 12:12:50 +0545

    Set up screen

--- pong.py
+++ pong.py
@@ -3,3 +3,8 @@ import sys
 
 # Initialize pygame
 pygame.init()
+
+# Screen dimensions
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
